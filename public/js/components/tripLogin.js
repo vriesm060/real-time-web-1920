e21871efcf5fe6f8a9ce5be06b20c7a3bb7764d9
Max de Vries <max.devries.1995@gmail.com>
2020-04-26 15:56:04 +0200

    Handle clients login

--- public/js/components/tripLogin.js
+++ public/js/components/tripLogin.js
@@ -1,6 +1,9 @@
 export default {
   overlay: document.querySelector('.login-overlay'),
   init: function (namespace) {
+    this.login = this.overlay.querySelector('.login');
+
+    // Listen to the trip's namespace:
     namespace
       .on('show login', () => {
         this.overlay.classList.add('show');
@@ -8,6 +11,20 @@ export default {
 
       .on('hide login', () => {
         this.overlay.classList.remove('show');
-      })
+      });
+
+    this.login.addEventListener('submit', (e) => {
+      var post = {};
+      var formdata = new FormData(e.target);
+
+      for (var key of formdata.keys()) {
+        post[key] = formdata.get(key);
+      }
+
+      // Add the client's username to the trip:
+      namespace.emit('post user', post);
+      this.overlay.classList.remove('show');
+      e.preventDefault();
+    });
   }
 };
